e6b0ba508ce33461cf14888baddd5e703e22b847
abogusz <aleksandrabogusz@protonmail.com>
2021-01-13 21:03:41 +0100

    work in progress

--- search-service/app.js
+++ search-service/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const solr = require('shared/solr-connection.js');
 
 const PORT = process.env.SEARCH_SERVICE_PORT;
 const app = express();
 
 app.get('/ping', (req, res) => {
-    res.send("pong");
+    res.send("ok");
 });
 
 app.listen(PORT, () => {
