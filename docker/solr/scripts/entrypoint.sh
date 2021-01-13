30a3b8c8a51c2c12d49a43d2b44ee2439ea06284
abogusz <aleksandrabogusz@protonmail.com>
2021-01-13 19:28:03 +0100

    work in progress

--- docker/solr/scripts/entrypoint.sh
+++ docker/solr/scripts/entrypoint.sh
@@ -3,4 +3,5 @@
 docker-entrypoint.sh -cloud &
 /userfiles/scripts/wait-for-it.sh -h localhost -p 8983 -t 15 &&
 /userfiles/scripts/create.sh &&
+/userfiles/scripts/upload-data.sh &&
 tail -f /dev/null
