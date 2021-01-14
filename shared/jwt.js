563c4dd195c321cd31a68f805cfb1e46b42e6e62
abogusz <aleksandrabogusz@protonmail.com>
2021-01-14 19:00:11 +0100

    added jwt.js module

---
+++ shared/jwt.js
@@ -0,0 +1,50 @@
+const _jwt = require('jsonwebtoken');
+
+const _SECRET = process.env.JWT_SECRET;
+
+const sign = (payload) => _jwt.sign(payload, _SECRET, { expiresIn: '1h' });
+const verify = (token) => {
+    try {
+        return _jwt.verify(token, _SECRET);
+    } catch (err) {
+        return null;
+    }
+}
+
+const middleware = (req, res, next) => {
+    const value = req.get('Authorization');
+    if (!value) {
+        return res
+            .status(403)
+            .json({ msg: "Missing authorization header." });
+    }
+
+    if (!value.startsWith('Bearer ')) {
+        return res
+            .status(403)
+            .json({ msg: "Unexpected 'Authorization' value format." });
+    }
+
+    const token = value.split('Bearer ')[1];
+    if (!token) {
+        return res
+            .status(403)
+            .json({ msg: "Malformed 'Authorization' value."});
+
+    }
+
+    const payload = verify(token);
+    if (!payload) {
+        return res
+            .status(403)
+            .json({ msg: "Invalid token." });
+    }
+
+    next();
+}
+
+module.exports = {
+    sign,
+    verify,
+    middleware
+}
