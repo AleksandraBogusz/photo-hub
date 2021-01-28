41641cd08160fd7c6e9906647daf355ce0ac84ba
abogusz <aleksandrabogusz@protonmail.com>
2021-01-28 18:05:41 +0100

    deleted unneccessarily added dirs

--- docker/down-test.sh
+++ docker/down-test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+rm -rf frontend/build
+rm -rf frontend/shared
+rm -rf authentication-service/build
+rm -rf authentication-service/shared
+rm -rf search-service/build
+rm -rf search-service/shared
+
 docker-compose --file docker-compose-test.yml down
